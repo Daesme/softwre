772b7595b054d85d99a598657d9de5e34e4bad94
USER <USER@Lenovo-PC>
2017-08-17 10:24:15 -0500

    mmmm

---
+++ web/ejercicio.js
@@ -0,0 +1,15 @@
+
+function cambiar(numero){
+    if (numero==1) {
+       document.getElementById("img").innerHTML="<img src='a.png' id='img' />"; 
+    }
+    if (numero==2) {
+       document.getElementById("img").innerHTML="<img src='b.png' id='img' />"; 
+    }
+    if (numero==3) {
+       document.getElementById("img").innerHTML="<img src='c.png' id='img' />"; 
+    }if (numero==4) {
+       document.getElementById("img").innerHTML="<img src='d.png' id='img' />"; 
+    }
+          
+}
